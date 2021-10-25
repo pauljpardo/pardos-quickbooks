598c2426a4deb9a3b776040be64e96248ecf4cac
Paul Pardo <paulpardo@Pauls-MacBook-Pro.local>
2021-10-25 00:01:28 -0400

    axios req

---
+++ pardos-quickbooks/src/App.js
@@ -0,0 +1,58 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
+/*
+
+Budget App
+  What is needed:
+    - input form to add expenses
+    - useEffect to GET request our existing expenses and populate them onto the page
+    - 
+
+Components: Form, Nav, Resources, About us page, footer, ExpenseList (inside app)
+State: 
+App
+  -GET 
+    -Render expenses
+ExpensePost
+    -Delete
+Form
+    - POST
+    - PMVP (PUT) request
+    - 
+ - 
+
+
+ - get request will be inside the useEffect 
+*/
+
+
+
+function App() {
+  const [expenseList, setExpenseList] = useState([])
+  
+
+  useEffect(() => {
+
+    const getExpenses = async () => {
+      const resp = await axios.get('https://api.airtable.com/v0/appIyBV0aECOah3WC/Table%201?api_key=keyp7EhQcdEAhoxyV');
+      console.log(resp.data.records)
+      setExpenseList(resp.data.records);
+    }
+
+    getExpenses();
+    console.log('Expense list')
+    
+  }, []);
+
+
+
+  return (
+    <div>
+      
+
+    </div>
+  );
+}
+
+export default App;
